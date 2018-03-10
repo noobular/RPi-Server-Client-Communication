5e6f3b6a1cfe3e9cf1567c30b739cf46ed4e6cb6
Noobular <noobularkid@gmail.com>
2018-03-10 09:46:33 -0500

    Test port

--- client.py
+++ client.py
@@ -2,7 +2,7 @@ import socket
 
 s = socket.socket()        
 host = '10.91.25.74'# ip of raspberry pi 
-port = 12345               
+port = "22"             
 s.connect((host, port))
 print(s.recv(1024))
 s.close()
--- server.py
+++ server.py
@@ -1,13 +1,18 @@
 import socket
+from gpiozero import LED #conversion from GPIO to GPIOZero
+
+R = LED(3) #GPIOZero's version of GPIO.setup
+G = LED(4) #GPIOZero's version of GPIO.setup
+B = LED(27) #GPIOZero's version of GPIO.setup
 
 s = socket.socket()
 host = '10.91.27.24' #ip of raspberry pi
-port = 12345
+port = "22"
 s.bind((host, port))
 
 s.listen(5)
 while True:
   c, addr = s.accept()
-  print ('Got connection from',addr)
+  R.on()
   c.send('Thank you for connecting')
   c.close()
