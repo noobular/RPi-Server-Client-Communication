599c6d2cd8f6d552a970149388b2a6b3f7698371
noobularkid <noobularkid@gmail.com>
2018-03-10 18:33:51 -0500

    Revision 1.07

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ try:
 	R = LED(3) #GPIOZero's version of GPIO.setup
 	G = LED(4) #GPIOZero's version of GPIO.setup
 	B = LED(27) #GPIOZero's version of GPIO.setup
-else:
+else
 	print("Failed to create LED information...")
 HOST = '192.168.1.16' # Server IP or Hostname
 PORT = 21012 #Pick an open Port (1000+ recommended), must match the client sport
