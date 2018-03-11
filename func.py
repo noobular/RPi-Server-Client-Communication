4d1ab45ec612a2936695e8b168525de3171642b8
noobularkid <noobularkid@gmail.com>
2018-03-10 20:47:27 -0500

    x

--- func.py
+++ func.py
@@ -1,16 +1,17 @@
 import config
 global reply
+
 def messageCheck(data):
     global reply
     reply = ''
-    if data == 'quit':
-        conn.send('Terminating'.encode())
-        return False
-    elif data == "1":
-        print("Test check 1")
-        reply = 'Test Check 1'
-    elif data == "2":
-        print("Test check 2")
-        reply = 'Test Check 2'
-    else:   
-        reply = 'Unknown command'
+if data == 'quit':
+    conn.send('Terminating'.encode())
+    return False
+elif data == "1":
+    print("Test check 1")
+    reply = 'Test Check 1'
+elif data == "2":
+    print("Test check 2")
+    reply = 'Test Check 2'
+else:   
+    reply = 'Unknown command'
