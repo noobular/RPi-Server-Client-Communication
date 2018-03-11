423f14a938a57f7e9016f1ee2c8153bdba3189a6
noobularkid <noobularkid@gmail.com>
2018-03-10 20:56:11 -0500

    x

--- func.py
+++ func.py
@@ -1,18 +1,19 @@
 import config
-global reply
-reply = ''
 
 def messageCheck(check):
-    global reply
     reply = ''
     if check == 'quit':
         conn.send('Terminating'.encode())
         return False
+    ##======INPUT CHECKS HERE=======##
     elif check == "1":
         print("Test check 1")
         reply = 'Test Check 1'
     elif check == "2":
         print("Test check 2")
         reply = 'Test Check 2'
+    ##^============================^##
     else:   
         reply = 'Unknown command'
+    return reply
+
