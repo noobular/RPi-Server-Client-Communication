a58ea886445c24a8267262e9e5a67199f5eea7e0
noobularkid <noobularkid@gmail.com>
2018-03-10 20:19:24 -0500

    x

--- func.py
+++ func.py
@@ -5,8 +5,10 @@ def messageCheck(data):
 	######## INPUT CHECKS HERE ##############	
 	elif data == "1":
 		print("Test check 1")
+		reply = 'Test Check 1'
 	elif data == "2":
 		print("Test check 2")
+		reply = 'Test Check 2'
 	#########################################
 	else:
 		reply = 'Unknown command'
