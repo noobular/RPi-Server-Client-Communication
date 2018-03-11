a332a7745980dcee8ad2c7632c05315e532f431f
noobularkid <noobularkid@gmail.com>
2018-03-10 21:28:21 -0500

    x

--- func.py
+++ func.py
@@ -1,18 +1,29 @@
 import config
+'''
+    This is how you would add a comment to the check..
+    elif check == "recieved message":
+        print("Print to the server console")
+        reply = 'Print to the client'
+        #Any other functions you'd like to add here.
+'''
 
+possibleCommands = "upload, download, help, commands"
 def messageCheck(check):
     reply = ''
     if check == 'quit':
         conn.send('Terminating'.encode())
         return False
     ##======INPUT CHECKS HERE=======##
-    elif check == "1":
-        print("Test check 1")
-        reply = 'Test Check 1'
-    elif check == "2":
-        print("Test check 2")
-        reply = 'Test Check 2'
+    elif check == "upload":
+        print("Attempting to upload data...")
+        reply = 'Server Attempting to upload file ....'
+    elif check == "download":
+        print("Attempting to send data to client")
+        reply = 'Server Attempting to send data...'
     ##^============================^##
+    elif check == "help" or check == "commands":
+        print("Sending Help Information")
+        reply = 'Possible commands:' + possibleCommands
     else:   
         reply = 'Unknown command'
     return reply
