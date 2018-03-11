08223677aafbcd39361dfafc076a6bc137d1628b
noobularkid <noobularkid@gmail.com>
2018-03-10 20:45:21 -0500

    x

--- func.py
+++ func.py
@@ -1,15 +1,15 @@
 global reply
 def messageCheck(data):
-	global reply
-	reply = ''
-	if data == 'quit':
-		conn.send('Terminating'.encode())
-		return False
-	elif data == "1":
-		print("Test check 1")
-		reply = 'Test Check 1'
-	elif data == "2":
-		print("Test check 2")
-		reply = 'Test Check 2'
-	else:	
-		reply = 'Unknown command'
+    global reply
+    reply = ''
+    if data == 'quit':
+        conn.send('Terminating'.encode())
+        return False
+    elif data == "1":
+        print("Test check 1")
+        reply = 'Test Check 1'
+    elif data == "2":
+        print("Test check 2")
+        reply = 'Test Check 2'
+    else:   
+        reply = 'Unknown command'
