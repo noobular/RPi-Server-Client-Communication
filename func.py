34396ea984c60a2a4b97ef706a7305ad1e6820eb
Noobular <noobularkid@gmail.com>
2018-05-16 13:05:37 -0400

    testyboi

--- func.py
+++ func.py
@@ -1,4 +1,6 @@
 import config
+from paramiko import SSHClient
+from scp import SCPClient
 '''
     This is how you would add a comment to the check..
     elif check == "recieved message":
@@ -18,7 +20,7 @@ def messageCheck(check):
     ##V=====INPUT CHECKS HERE======V##
     elif check == "upload":
         print("$$ Attempting to upload data...")
-        svr_uploadfile()
+        'svr_uploadfile()
         reply = 'Server Attempting to upload file ....'
     elif check == "download":
         print("$$ Attempting to send data to client")
@@ -28,13 +30,35 @@ def messageCheck(check):
     elif check == "help" or check == "commands":
         print("$$ Sending Help Information")
         reply = 'Possible commands:' + possibleCommands
-    else:   
+    else:
         reply = 'Unknown command'
     return reply
 
 ### ADD YOUR FUNCTIONS AFTER THIS LINE ###
+def createSSHClient(server, port, user, password):
+    client = paramiko.SSHClient()
+    client.load_system_host_keys()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(server, port, user, password)
+    return client
 
 def svr_uploadfile():
+    ssh = createSSHClient(server, port, user, password)
+    ssh = SSHClient()
+    ssh.load_system_host_keys()
+    ssh.connect('example.com') 'IP Address for Server
+
+    # SCPCLient takes a paramiko transport as an argument
+    scp = SCPClient(ssh.get_transport())
+
+    scp.put('test.txt', 'test2.txt')
+    scp.get('test2.txt')
+
+    # Uploading the 'test' directory with its content in the
+    # '/home/user/dump' remote directory
+    scp.put('test', recursive=True, remote_path='/home/user/dump')
+
+    scp.close()
     print("$$ Test upload function ran.")
 
 def cli_downloadfile():
